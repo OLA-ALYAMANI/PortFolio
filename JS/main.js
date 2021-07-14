747b70b4363d67b8f18ae1253aa46ff4c8b5e589
OLA13 <o-m-yamani@hotmail.com>
2021-07-14 22:21:35 +0300

    contact area

--- JS/main.js
+++ JS/main.js
@@ -130,11 +130,11 @@ $(document).ready(function(){
 const tabsContainer = document.querySelector(".about-table"), aboutSection = document.querySelector(".edu-exp-area");
 
 tabsContainer.addEventListener("click", (e) => {
-    if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
-        tabsContainer.querySelector(".active").classList.remove("active");
-        e.target.classList.add("active");
+    if(e.target.classList.contains("tab-item") && !e.target.classList.contains('active')){
+        tabsContainer.querySelector(".active").classList.remove('active');
+        e.target.classList.add('active');
         const target = e.target.getAttribute("data-target");
-        aboutSection.querySelector(".tab-content.active").removeClass;
-        aboutSection.querySelector(target).addClass;
+        aboutSection.querySelector(".tab-content.active").classList.remove('active');
+        aboutSection.querySelector(target).classList.add('active');
     }
 })
